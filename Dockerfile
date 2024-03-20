9abc8b41f7580268d9b3b227235f69dfd7c0f284
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2024-03-20 22:29:59 +0100

    awd

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,4 @@ FROM ubuntu:latest
 COPY . .
 RUN apt-get -y update
 RUN apt install -y default-jre
-WORKDIR "/Bungee"
-CMD java -jar Bungee.jar
+CMD ["bash", "main.sh"]
---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+<<<<<<< HEAD
+sudo apt install default-jre
+=======
+sudo apt install default-jre
+>>>>>>> f31406d6298ba933af26178b3eb966c78d992ad4
